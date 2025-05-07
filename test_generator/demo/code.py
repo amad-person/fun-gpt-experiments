fae6a06cb6391118204ea3a65057ae29e220ecd9
Aadyaa Maddi <22164211+amad-person@users.noreply.github.com>
2025-05-06 22:44:18 -0400

    Add FakerValues generator (#19)

--- test_generator/demo/code.py
+++ test_generator/demo/code.py
@@ -8,3 +8,11 @@ def compute_mean(x):
 def compute_elementwise_sum(x, y):
     assert len(x) == len(y)
     return np.sum([x, y], axis=0)
+
+
+def concat_elementwise_strings(x, y, delim=";"):
+    assert len(x) == len(y)
+    result = []
+    for x_i, y_i in zip(x, y):
+        result.append(x_i + delim + y_i)
+    return result
